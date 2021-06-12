8c1a57691634af32dfb5c2ea0267325231800806
Mithun-Ghosh-Mishu <85070201+Mithun-Ghosh-Mishu@users.noreply.github.com>
2021-06-12 23:41:14 +0600

    Project File Created & upload initial Code

---
+++ Student  Result Management System.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    cout<<endl<<endl;
+    cout<<"\t\t\t       ##############################################################"<<endl;
+    cout<<endl;
+    cout<<"\t\t\t\t\t     Welcome To Our Result ManageMent System"<<endl;
+    cout<<endl;
+    cout<<"\t\t\t       ##############################################################"<<endl;
+    cout<<endl<<endl;
+
+    int choice;
+    cout<<"\t\t\t\t\t           1.Student"<<endl<<endl;
+    cout<<"\t\t\t\t\t           2.Admin"<<endl<<endl;
+    cout<<"\t\t\t\t\t           3.Close Application"<<endl<<endl;
+    cout<<"\t\t\t\t\t           Choice Your Option: ";
+    cin>>choice;
+
+}
